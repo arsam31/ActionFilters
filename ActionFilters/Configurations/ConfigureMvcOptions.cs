d6fd12cf331ecc982777b9bc35e9c4d911ee3eba
arsam <arsamrahmaan31@gmail.com>
2021-12-20 13:38:40 +0500

    Add project files.

---
+++ ActionFilters/Configurations/ConfigureMvcOptions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace ActionFilters.Configurations
+{
+ 
+        internal class ConfigureMvcOptions : IConfigureOptions<MvcOptions>
+        {
+            private readonly IHostingEnvironment _env;
+            public ConfigureMvcOptions(IHostingEnvironment env)
+            {
+                _env = env;
+            }
+
+            public void Configure(MvcOptions options)
+            {
+                if (_env.IsDevelopment())
+                {
+                    options.SslPort = 44523;
+                }
+                else
+                {
+                    options.Filters.Add(new RequireHttpsAttribute());
+                }
+            }
+        }
+    }
+
